438fb50dc94bc410f21e691ed3f31e635032c184
daybreak124 <ardayeet52@gmail.com>
2026-08-12 20:58:44 +0300

    different authorization for each http req for products db

--- src/main/java/arda/xmlproject/demo/entities/ApiPermissions.java
+++ src/main/java/arda/xmlproject/demo/entities/ApiPermissions.java
@@ -1,5 +1,12 @@
 package arda.xmlproject.demo.entities;
 
 public enum ApiPermissions {
-    COUNTRY, NUMBER, TEMPERATURE, ISBN, CALCULATOR
+    COUNTRY, NUMBER, TEMPERATURE, ISBN, CALCULATOR, USER, PRODUCTS,
+    USER_ADM,
+
+
+    PRODUCTS_ADM, PRODUCTS_DEL, PRODUCTS_PATCH,
+    PRODUCTS_POST, PRODUCTS_GET
+
+    // Bazı yetkiler obsolete, krakend json dosyası ve spring security anotasyonları kontrol edilerek silinmeli
 }
